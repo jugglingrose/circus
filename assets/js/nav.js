ae195f8da96b01fb12eadb1699022788f6cc75f4
Corean Gonzales <coreangonzales@jubilee.local>
2026-03-05 18:04:27 -0600

    landing page updates and removal of nav color changing javascript

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,42 +1,46 @@
-'use strict'
+"use strict";
 
 //Vanilla JS//
 /* When scrollTop === 0, set nav bar to white for media query < 760 width and
 set nav bar to transparent for media query > 760 width */
-function getSize(){
-  var w = document.documentElement.clientWidth;
-  var nav = document.getElementById('navb');
+function getSize() {
+  return;
+  // var w = document.documentElement.clientWidth;
+  // var nav = document.getElementById('navb');
 
-  if ( w >= 760){
-    nav.classList.remove('redNav');
-    nav.classList.add('transparentNav');
-  } else{
-    nav.classList.remove('transparentNav');
-    nav.classList.add('redNav');
-  }
-  scrollW(w, nav);
+  // if ( w >= 760){
+  //   nav.classList.remove('redNav');
+  //   nav.classList.add('transparentNav');
+  // } else{
+  //   nav.classList.remove('transparentNav');
+  //   nav.classList.add('redNav');
+  // }
+  // scrollW(w, nav);
 }
 
 /*on page scroll, set nav bar to red when width > 760 && once user has
 scrolled beyond the hero landing*/
-function scrollW(w, nav){
-  window.addEventListener("scroll", function(){
-  var top = this.scrollY;
-  var a = document.getElementById('about');
-  var aboutTop = a.offsetTop;
+function scrollW(w, nav) {
+  console.log(w, nav);
+  return;
+  // window.addEventListener("scroll", function () {
+  //   var top = this.scrollY;
+  //   var a = document.getElementById("about");
+  //   var aboutTop = a.offsetTop;
 
-  if ( w < 760){
-    nav.classList.remove('transparentNav');
-    nav.classList.add('redNav');
-  }
-  else if ( w > 760){
-    if(top < aboutTop){
-      nav.classList.remove('redNav');
-      nav.classList.add('transparentNav');
-    }else if ( top > aboutTop ) {
-      nav.classList.remove('transparentNav');
-      nav.classList.add('redNav');
-    }
-  }
-  })
+  //   if (w < 760) {
+  //     nav.classList.remove("transparentNav");
+  //     nav.classList.add("redNav");
+  //   } else if (w > 760) {
+  //     nav.classList.remove("redNav");
+  //     nav.classList.add("transparentNav");
+  //     // if (top < aboutTop) {
+  //     //   nav.classList.remove("redNav");
+  //     //   nav.classList.add("transparentNav");
+  //     // } else if (top > aboutTop) {
+  //     //   nav.classList.remove("transparentNav");
+  //     //   nav.classList.add("redNav");
+  //     // }
+  //   }
+  // });
 }
